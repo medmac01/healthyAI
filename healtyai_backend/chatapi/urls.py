37ae54982966e74b551e27060285716bad5cf0cb
hsaine <hssainayoub77@gmail.com>
2023-03-01 00:32:59 +0100

    bug fix

--- healtyai_backend/chatapi/urls.py
+++ healtyai_backend/chatapi/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import get_response, message
+from .views import get_response, message, login
 
 urlpatterns = [
     path('message/', message, name='message'),
