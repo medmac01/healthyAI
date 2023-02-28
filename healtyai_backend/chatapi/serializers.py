8fb77ceacd84794d3855dd8ed87c2f57630d81fe
[Muhammed Machrouh] <[th3justhacker@gmail.com]>
2023-02-28 20:40:32 +0100

    Removed Serializers

--- healtyai_backend/chatapi/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Message
-
-class MessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Message
-        fields = ['id', 'text', 'timestamp']
-
